afa696084c781a9fb4623b7145dd8d08ec21a2ba
Laurent FABRE <laurent_psychedelic@hotmail.com>
2014-11-08 04:34:42 +0900

    Added "+" operation on vectors

--- include/geometry/Vector3D.hpp
+++ include/geometry/Vector3D.hpp
@@ -9,13 +9,38 @@ public:
     const T x;
     const T y;
     const T z;
+
     Vector3D(T x, T y, T z) : x(x), y(y), z(z) {}
     template <typename U>
-    friend std::ostream& operator<<(std::ostream& out, Vector3D<U>& instance);
+    Vector3D(Vector3D<U>& vector) : x(vector.x), y(vector.y), z(vector.z) {}
+
+    template <typename U>
+    const Vector3D<U> operator+(Vector3D<U>& vector) const;
+
+    template <typename U>
+    const Vector3D<U> operator+=(Vector3D<U>& vector) const;
+
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& out, const Vector3D<U>& instance);
 };
 
 template <typename T>
-std::ostream& operator<<(std::ostream& out, Vector3D<T>& instance) {
+template <typename U>
+const Vector3D<U> Vector3D<T>::operator+(Vector3D<U>& vector) const {
+    return Vector3D<U>(*this) += vector;
+}
+
+template <typename T>
+template <typename U>
+const Vector3D<U> Vector3D<T>::operator+=(Vector3D<U>& vector) const {
+    const U x = this->x + vector.x;
+    const U y = this->y + vector.y;
+    const U z = this->z + vector.z;
+    return Vector3D<U>(x, y, z);
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Vector3D<T>& instance) {
     return out << "Vector3D: (" << instance.x << "," << instance.y << "," << instance.z << ") @{" << &instance << "}";
 }
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -7,7 +7,8 @@ using namespace std;
 #define Vector Vector3D<double>
 
 int main(int argc, char *argv[]) {
-    Vector vector(1.234, 2.345, 3.456);
-    cout << vector << endl;
+    Vector vector1(1.234, 2.345, 3.456);
+    Vector vector2(1,2,3);
+    cout << (vector1 + vector2) << endl;
     return 0;
 }
