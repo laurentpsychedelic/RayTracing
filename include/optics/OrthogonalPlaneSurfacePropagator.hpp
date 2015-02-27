f2a3b0351d9c5d4731386c6a3b8f4f00634af738
Laurent FABRE <laurent_psychedelic@hotmail.com>
2015-02-27 10:40:54 +0900

    Added the propagator for planar surfaces

---
+++ include/optics/OrthogonalPlaneSurfacePropagator.hpp
@@ -0,0 +1,30 @@
+#ifndef ORTHOGONALPLANESURFACEPROPAGATOR_HPP
+#define ORTHOGONALPLANESURFACEPROPAGATOR_HPP
+
+#include "geometry/OrthogonalPlaneSurface3D.hpp"
+#include "geometry/Utils.hpp"
+#include "optics/IPropagator.hpp"
+
+template <typename T, typename U>
+class OrthogonalPlaneSurfacePropagator : public IPropagator<T, U> {
+public:
+    /* Member functions */
+    virtual const Ray<T, U> propagate(const Ray<T, U>& ray, ISurface3D<T>* surface);
+    /* Virtual destructor */
+    virtual ~OrthogonalPlaneSurfacePropagator() {}
+};
+
+template <typename T, typename U>
+const Ray<T, U> OrthogonalPlaneSurfacePropagator<T, U>::propagate(const Ray<T, U>& ray, ISurface3D<T>* _surface) {
+    if (ray.direction.z <= 0) // The ray is lost
+        return Ray<T, U>(ray.location, ray.direction, 0.0); // 0 intensity
+    const OrthogonalPlaneSurface3D<T>* surface = dynamic_cast<OrthogonalPlaneSurface3D<T>* >(_surface);
+    if (!surface)
+        throw "Surface is not a planar surface!! (orthogonal plane surface propagator)";
+    const Vector3D<T> direction = ray.direction;
+    const Point3D<T> start = ray.location;
+    Point3D<T> prop = start + (surface->z - start.z) * !direction;
+    return Ray<T, U>(prop, direction, ray.intensity);
+}
+
+#endif
--- src/Main.cpp
+++ src/Main.cpp
@@ -15,6 +15,7 @@
 #include "optics/ObjectDiffuser.hpp"
 #include "optics/Object3D.hpp"
 #include "optics/PointSource3D.hpp"
+#include "optics/OrthogonalPlaneSurfacePropagator.hpp"
 #include "optics/SphericalSurfacePropagator.hpp"
 
 using namespace std;
@@ -39,6 +40,7 @@ using namespace std;
 #define Range3D Range3D<COORD_TYPE>
 #define Range Range<COORD_TYPE>
 #define Ray Ray<COORD_TYPE, INTENS_TYPE>
+#define PlanarSurfacePropagator OrthogonalPlaneSurfacePropagator<COORD_TYPE, INTENS_TYPE>
 #define SphericalSurfacePropagator SphericalSurfacePropagator<COORD_TYPE, INTENS_TYPE>
 
 int main(int argc, char *argv[]) {
@@ -126,13 +128,17 @@ int main(int argc, char *argv[]) {
         for (vector<Ray>::iterator it = rays.begin() ; it != rays.end(); ++it)
             cout << *it << endl;
 
-        PointSource ps(Point(0.0, 0.0, 0.0) /* location */, 1.0 /* intensity */);
-        Ray rin(ps.location, Vector(0.0, 0.0, 1.0), 1.0);
+        PointSource ps(Point(0.0, 0.0, -100.0) /* location */, 1.0 /* intensity */);
+        Ray rin(ps.location, !(Vector(0.01, 0.02, 1.0)), 1.0);
         ISurface* issurf = &( dynamic_cast<ISurface&>(ssurf) );
         SphericalSurfacePropagator ssp;
-        Ray rout = ssp.propagate(rin, issurf);
+        Ray rout = rin;//ssp.propagate(rin, issurf);
         cout << "Source ray: " << rin << endl;
         cout << "Propagation to: " << rout << endl;
+        issurf = &( dynamic_cast<ISurface&>(psurf) );
+        PlanarSurfacePropagator psp;
+        Ray rout2 = psp.propagate(rout, issurf);
+        cout << "Propagation to: " << rout2 << endl;
     } catch (const char* error) {
         cout << "ERROR! >> " << error << endl;
         return -1;
